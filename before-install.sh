c03fcb750933d68ef75e59cf30684d1978b07406
Daniel Kopecký <kopecky.d@gmail.com>
2021-04-06 11:49:48 +0200

    Disabled pip-compile verbose

--- before-install.sh
+++ before-install.sh
@@ -9,5 +9,5 @@
 pip install --upgrade pip setuptools py
 pip install twine wheel coveralls requirements-builder pip-tools
 requirements-builder -e $EXTRAS --level=pypi setup.py > .travis-release-requirements.in
-pip-compile -U --verbose -o .travis-release-requirements.txt .travis-release-requirements.in
+pip-compile -U -o .travis-release-requirements.txt .travis-release-requirements.in
 cat .travis-release-requirements.txt
