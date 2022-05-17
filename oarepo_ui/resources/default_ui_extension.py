69ac9f5829c40c9c763d772dd105b7b91d080999
Mirek Simek <miroslav.simek@gmail.com>
2022-05-17 14:38:40 +0200

    component file refactor

--- oarepo_ui/resources/default_ui_extension.py
+++ oarepo_ui/resources/default_ui_extension.py
@@ -5,5 +5,8 @@ class DefaultUIExtensionConfig(OARepoUIExtensionConfig):
     imported_templates = {
         'oarepo_ui': 'oarepo_ui/macros.html.jinja2',
         'oarepo_ui_components': 'oarepo_ui/components/components.html.jinja2',
+        'oarepo_ui_grid': 'oarepo_ui/components/structural/grid.html.jinja2',
+        'oarepo_ui_row': 'oarepo_ui/components/structural/row.html.jinja2',
+        'oarepo_ui_column': 'oarepo_ui/components/structural/column.html.jinja2',
         'authority': 'oarepo_ui/components/authority.html.jinja2'
     }
