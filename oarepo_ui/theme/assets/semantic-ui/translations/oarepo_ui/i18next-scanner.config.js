10f714a6917feb491b5e0d929687f7faff8a6ef7
Miroslav Bauer <mirekys@users.noreply.github.com>
2022-06-24 10:16:30 +0200

    fix some smells

--- oarepo_ui/theme/assets/semantic-ui/translations/oarepo_ui/i18next-scanner.config.js
+++ oarepo_ui/theme/assets/semantic-ui/translations/oarepo_ui/i18next-scanner.config.js
@@ -6,9 +6,9 @@
 
 // list of func used to
 // mark the strings for translation
-const { languages } = require("./package.json").config;
-const funcList = ["i18next.t"];
-const extensions = ['.js', '.jsx'];
+const { languages } = require('./package.json').config
+const funcList = ['i18next.t']
+const extensions = ['.js', '.jsx']
 
 module.exports = {
   options: {
@@ -19,40 +19,40 @@ module.exports = {
       list: funcList,
       extensions: extensions,
     },
-      //using Trans component
+    //using Trans component
     trans: {
       component: 'Trans',
       extensions: extensions,
-      fallbackKey: function (ns, value) {
-        return value;
+      fallbackKey: function (_ns, value) {
+        return value
       },
     },
     lngs: languages,
     ns: [
       // file name (.json)
-      "translations",
+      'translations',
     ],
-    defaultLng: "en",
-    defaultNs: "translations",
+    defaultLng: 'en',
+    defaultNs: 'translations',
     // @param {string} lng The language currently used.
     // @param {string} ns The namespace currently used.
     // @param {string} key The translation key.
     // @return {string} Returns a default value for the translation key.
-    defaultValue: function (lng, ns, key) {
-      if (lng === "en") {
+    defaultValue: function (lng, _ns, key) {
+      if (lng === 'en') {
         // Return key as the default value for English language
-        return key;
+        return key
       }
-      return "";
+      return ''
     },
     resource: {
       // The path where resources get loaded from. Relative to current working directory.
-      loadPath: "messages/{{lng}}/{{ns}}.json",
+      loadPath: 'messages/{{lng}}/{{ns}}.json',
 
       // The path to store resources.
-      savePath: "messages/{{lng}}/{{ns}}.json",
+      savePath: 'messages/{{lng}}/{{ns}}.json',
       jsonIndent: 2,
-      lineEnding: "\n",
+      lineEnding: '\n',
     },
     nsSeparator: false, // namespace separator
 
@@ -60,4 +60,4 @@ module.exports = {
     // if you prefer having keys as the fallback for translation (e.g. gettext).
     keySeparator: false,
   },
-};
+}
