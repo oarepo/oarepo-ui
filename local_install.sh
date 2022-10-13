ef2f25132739c0d889efd66a04f4e7a7414bf01d
Miroslav Bauer <mirekys@users.noreply.github.com>
2022-10-13 15:36:30 +0000

    fix test deps

--- local_install.sh
+++ local_install.sh
@@ -8,3 +8,8 @@ source .venv/bin/activate
   cd oarepo-ui
   pip install -e '.[tests]'
 )
+
+(
+  cd oarepo-ui-model-builder
+  pip install -e '.[tests]'
+)
--- oarepo-ui-model-builder/oarepo_ui_model_builder/__init__.py
+++ oarepo-ui-model-builder/oarepo_ui_model_builder/__init__.py
@@ -1,2 +1,2 @@
 __version__ = "1.0.3"
-__all__ = ('__version__')
+__all__ = ('__version__',)
