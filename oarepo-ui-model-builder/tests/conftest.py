c27aae0c8d8faba5b1eb4550324d79317049d3de
Miroslav Bauer <mirekys@users.noreply.github.com>
2022-10-14 10:47:56 +0000

    update test entrypoints

--- oarepo-ui-model-builder/tests/conftest.py
+++ oarepo-ui-model-builder/tests/conftest.py
@@ -1,16 +1,18 @@
 import pytest
 from invenio_app.factory import create_api as _create_api
 
+
 @pytest.fixture(scope="module")
 def extra_entry_points():
     """Extra entry points to load the mock_module features."""
     return {
         "oarepo_model_builder_ui.components": [
-            "row = mock_module.components:RowComponent",
-            "list = mock_module.components:ListComponent",
-            "column = mock_module.components:ColumnComponent"
+            "row = oarepo_ui_model_builder.ui_components_specs:RowComponent",
+            "list = oarepo_ui_model_builder.ui_components_specs:ListComponent",
+            "column = oarepo_ui_model_builder.ui_components_specs:ColumnComponent"
         ],
     }
+
 
 @pytest.fixture(scope="module")
 def create_app(instance_path, entry_points):
