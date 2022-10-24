405d24eeaffadba3bfbf09c74c5997ed62a1dfe7
Miroslav Bauer <mirekys@users.noreply.github.com>
2022-10-24 14:10:43 +0000

    remove resolved TODO

--- oarepo-ui/oarepo_ui/theme/assets/semantic-ui/js/oarepo_ui/index.js
+++ oarepo-ui/oarepo_ui/theme/assets/semantic-ui/js/oarepo_ui/index.js
@@ -14,14 +14,3 @@
 export { buildUID } from './util'
 export { useLayout } from './hooks'
 export { GlobalDataContextProvider } from './context'
-
-// TODO: update oarepo-ui README with info how to use this JS package:
-// 
-// import { useLayout } from '@js/oarepo_ui'
-// const appRoot = document.querySelector('#search-app')
-// const SearchLayout = ({ layout, data, useGlobalData = false }) => useLayout({ layout, data, useGlobalData })
-//
-// ReactDOM.render(
-//   <SearchLayout layout={searchResultItem} data={searchResultItemData} />
-//   appRoot,
-// )
