984d1ab85ff4e1b5bc656ffe6829223b97501bbe
Mirek Simek <miroslav.simek@gmail.com>
2023-03-03 22:38:03 +0100

    ..

--- oarepo-model-builder-ui/run-tests.sh
+++ oarepo-model-builder-ui/run-tests.sh
@@ -13,3 +13,15 @@ if [ -d tests/mock_record ] ; then
 fi
 
 .venv-builder/bin/oarepo-compile-model tests/model.json --output-directory tests/mock_record -vvv
+
+if [ -d .venv ] ; then 
+    rm -rf .venv
+fi
+
+python3 -m venv .venv
+
+source .venv/bin/activate
+pip install -e 'tests/mock_record'
+pip install pytest-invenio invenio-app 'invenio-search[opensearch2]' flask-babelex
+
+pytest tests
--- oarepo-model-builder-ui/tests/conftest.py
+++ oarepo-model-builder-ui/tests/conftest.py
@@ -1,20 +1,21 @@
 import pytest
-from invenio_app.factory import create_api as _create_api
+from invenio_app.factory import create_app as _create_app
 
 
 @pytest.fixture(scope="module")
 def extra_entry_points():
     """Extra entry points to load the mock_module features."""
-    return {
-        "oarepo_model_builder_ui.components": [
-            "row = oarepo_ui_model_builder.ui_components_specs:RowComponent",
-            "list = oarepo_ui_model_builder.ui_components_specs:ListComponent",
-            "column = oarepo_ui_model_builder.ui_components_specs:ColumnComponent"
-        ],
-    }
+    return {}
+
+
+@pytest.fixture(scope="module")
+def app_config(app_config):
+    app_config["I18N_LANGUAGES"] = [("en", "English"), ("cs", "Czech")]
+    app_config["BABEL_DEFAULT_LOCALE"] = "en"
+    return app_config
 
 
 @pytest.fixture(scope="module")
 def create_app(instance_path, entry_points):
     """Application factory fixture."""
-    return _create_api
+    return _create_app
