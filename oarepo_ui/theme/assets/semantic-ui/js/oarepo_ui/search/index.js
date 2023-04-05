f0485f44aa91a6fba01f872c91511ca876bd53fb
Miroslav Bauer <mirekys@users.noreply.github.com>
2023-04-05 17:12:19 +0200

    feat(search): :lipstick: add customizable search app layout components

--- oarepo_ui/theme/assets/semantic-ui/js/oarepo_ui/search/index.js
+++ oarepo_ui/theme/assets/semantic-ui/js/oarepo_ui/search/index.js
@@ -4,7 +4,10 @@ export { BucketAggregationValuesElement } from "./BucketAggregationValuesElement
 export { CountElement } from "./CountElement";
 export { ErrorElement } from "./ErrorElement";
 
+
 export { SearchAppFacets } from "./SearchAppFacets";
+export { SearchAppLayout } from "./SearchAppLayout";
+export { SearchAppResultOptions } from "./SearchAppResultOptions";
 export { SearchAppSearchbarContainer } from "./SearchAppSearchbarContainer";
 export { SearchAppSort } from "./SearchAppSort"
 export { SearchFiltersToggleElement } from "./SearchFiltersToggleElement";
