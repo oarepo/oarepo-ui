b1eac90e1e9e573a383c19d977e2658c254b8bcb
Dusanst <stojanovic@cesnet.cz>
2023-07-30 11:51:50 +0200

    minor changes

--- oarepo_ui/theme/assets/semantic-ui/js/oarepo_ui/forms/components/RelatedSelectField/ExternalApiResultsList.jsx
+++ oarepo_ui/theme/assets/semantic-ui/js/oarepo_ui/forms/components/RelatedSelectField/ExternalApiResultsList.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Header, Radio, Grid, Checkbox } from "semantic-ui-react";
+import { Header, Radio, Grid, Checkbox, Icon, Label } from "semantic-ui-react";
 import { withState } from "react-searchkit";
 import PropTypes from "prop-types";
 import Overridable from "react-overridable";
@@ -27,23 +27,20 @@ export const ExternalApiResultsList = withState(
         externalApiRecords={externalApiRecords}
         serializeExternalApiSuggestions={serializeExternalApiSuggestions}
       >
-        <Grid celled>
+        <Grid celled columns={3}>
           {serializeExternalApiSuggestions(results?.data?.hits).map(
             (record) => {
               const title = record.text;
+              const isChecked = externalApiRecords.some(
+                (record) => record.text === title
+              );
+              const isDisabled = externalApiRecords.length >= 20 && !isChecked;
               return multiple ? (
-                <Grid.Row
-                  key={title}
-                  // onClick={() => {
-                  //   handleAddingExternalApiSuggestion(record);
-                  //   handleExternalRecordChange([...externalApiRecords, record]);
-                  // }}
-                >
+                <Grid.Row width={10} key={title}>
                   <Grid.Column width={2}>
                     <Checkbox
-                      checked={externalApiRecords.some(
-                        (record) => record.text === title
-                      )}
+                      disabled={isDisabled}
+                      checked={isChecked}
                       onChange={() => {
                         handleExternalRecordChange(record);
                       }}
@@ -77,6 +74,21 @@ export const ExternalApiResultsList = withState(
                 </Grid.Row>
               );
             }
+          )}
+          {externalApiRecords.length > 0 && multiple && (
+            <Grid.Column width={10}>
+              <Header as="h3">Selected records</Header>
+              {externalApiRecords.map((record) => (
+                <Label
+                  image
+                  key={record.id}
+                  onClick={() => handleExternalRecordChange(record)}
+                >
+                  {record.text}
+                  <Icon name="delete" />
+                </Label>
+              ))}
+            </Grid.Column>
           )}
         </Grid>
       </Overridable>
