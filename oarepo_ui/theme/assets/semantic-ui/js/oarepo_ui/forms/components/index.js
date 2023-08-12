8f9452ed6628fc951b92e0d49e095a73b07722ec
Dusanst <stojanovic@cesnet.cz>
2023-08-12 14:09:08 +0200

    fixed export of MultiLingualtextInput

--- oarepo_ui/theme/assets/semantic-ui/js/oarepo_ui/forms/components/index.js
+++ oarepo_ui/theme/assets/semantic-ui/js/oarepo_ui/forms/components/index.js
@@ -2,3 +2,4 @@ export * from "./RelatedSelectField";
 export * from "./I18nString";
 export * from "./I18nTextInputField";
 export * from "./LanguageSelectField";
+export * from "./MultilingualTextInput";
