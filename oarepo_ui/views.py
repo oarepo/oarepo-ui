d0bc1b5c197ab53f28035d5c9232fa7a144833c8
Mirek Simek <miroslav.simek@gmail.com>
2023-09-15 12:25:46 +0200

    Added default serializer to allow serialization of i18n strings into … (#83)
    * Added default serializer to allow serialization of i18n strings into search config
    
    * Added the default filter to all other pages as well

--- oarepo_ui/views.py
+++ oarepo_ui/views.py
@@ -10,6 +10,7 @@ def add_jinja_filters(state):
     # TODO: modified the global env - not pretty, but gets filters to search as well
     env = ext.templates.jinja_env
     env.filters.update(ext.app.config["OAREPO_UI_JINJAX_FILTERS"])
+    env.policies.setdefault("json.dumps_kwargs", {}).setdefault("default", str)
 
 
 blueprint.record_once(add_jinja_filters)
