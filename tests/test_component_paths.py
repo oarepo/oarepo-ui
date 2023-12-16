0bc8cd207f5811f78231fad2fa5bf75501d3dd75
Mirek Simek <miroslav.simek@gmail.com>
2023-12-16 16:09:20 +0100

    Corrected handling of namespaced jinjax macros, enhanced macro resolution (both components.Field and Field can be used), working on code coverage

---
+++ tests/test_component_paths.py
@@ -0,0 +1,7 @@
+from oarepo_ui.proxies import current_oarepo_ui
+
+
+def test_component_paths(app):
+    component_paths = current_oarepo_ui.catalog.component_paths
+    assert 'components.Field' in component_paths
+    assert 'Field' in component_paths
