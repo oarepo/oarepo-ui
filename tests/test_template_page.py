358f03b18227959ab6c059c492c549410e3b56fe
Mirek Simek <miroslav.simek@gmail.com>
2023-12-29 17:38:53 +0100

    Fixed sonarcloud issue

--- tests/test_template_page.py
+++ tests/test_template_page.py
@@ -4,7 +4,7 @@ import json
 def test_template_page(
     app, titlepage_ui_resource, client, fake_manifest
 ):
-    with client.get(f"/") as c:
+    with client.get("/") as c:
         assert c.status_code == 200
         data = json.loads(c.text)
         assert 'ok' in data
