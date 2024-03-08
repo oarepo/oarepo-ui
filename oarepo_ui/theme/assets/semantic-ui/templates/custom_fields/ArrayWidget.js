b15f27a94582a0e6d23bbc3d0fe9e8563d350511
ducica <dusanstojanovic256@gmail.com>
2024-03-08 11:25:39 +0100

    fixed alignment

--- oarepo_ui/theme/assets/semantic-ui/templates/custom_fields/ArrayWidget.js
+++ oarepo_ui/theme/assets/semantic-ui/templates/custom_fields/ArrayWidget.js
@@ -1,12 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { Grid, Form, Button, Icon, Header } from "semantic-ui-react";
-import { useFormikContext, getIn, setIn } from "formik";
+import { Grid, Button, Icon, Header, Form } from "semantic-ui-react";
+import { useFormikContext, getIn } from "formik";
 import { importTemplate } from "./ComplexWidget";
+import PropTypes from "prop-types";
+import { GroupField } from "react-invenio-forms";
 
-export const ArrayWidget = ({ item_widget, item_props, item_initial_value, fieldPath, label }) => {
-  const formik = useFormikContext();
+export const ArrayWidget = ({
+  item_widget,
+  item_props,
+  item_initial_value,
+  fieldPath,
+  label,
+}) => {
+  const { values, setFieldValue } = useFormikContext();
 
-  const existingValues = getIn(formik.values, fieldPath, []);
+  const existingValues = getIn(values, fieldPath, []);
 
   const [importedComponent, setImportedComponent] = useState(null);
 
@@ -16,23 +24,29 @@ export const ArrayWidget = ({ item_widget, item_props, item_initial_value, field
       const ItemWidgetComponent = await importTemplate(item_widget);
       setImportedComponent({
         // can not return function here as it would be interpreted by the setter immediatelly
-        component: (idx) => <ItemWidgetComponent fieldPath={`${fieldPath}[${idx}]`} {...item_props} />
+        component: (idx) => (
+          <ItemWidgetComponent
+            fieldPath={`${fieldPath}[${idx}]`}
+            {...item_props}
+          />
+        ),
       });
     };
-    importComponent().catch((e) => { console.error(e); });
+    importComponent().catch((e) => {
+      console.error(e);
+    });
   }, []);
-
 
   const handleRemove = (indexToRemove) => {
     const updatedValues = [...existingValues];
     updatedValues.splice(indexToRemove.value, 1);
-    formik.setFieldValue(fieldPath, updatedValues);
+    setFieldValue(fieldPath, updatedValues);
   };
 
   const handleAdd = () => {
     const newIndex = existingValues.length;
     const newFieldPath = `${fieldPath}[${newIndex}]`;
-    formik.setFieldValue(newFieldPath, item_initial_value !== undefined ? item_initial_value : null);
+    setFieldValue(newFieldPath, item_initial_value ?? null);
   };
 
   if (!importedComponent) {
@@ -41,39 +55,51 @@ export const ArrayWidget = ({ item_widget, item_props, item_initial_value, field
 
   return (
     <>
-    <Header as="h3">{label}</Header>
-    <Grid>
-      {existingValues.map((value, index) =>
-        <Grid.Row key={index}>
-          <Grid.Column width="1">
+      <Header as="h3">{label}</Header>
+      <Grid>
+        <Grid.Column width={16}>
+          {existingValues.map((value, index) => (
+            <GroupField width={16} verticalAlign="middle" key={index}>
+              <Form.Field width={15}>
+                {importedComponent.component(index)}
+              </Form.Field>
+              <Form.Field>
+                <Button
+                  aria-label={"Remove field"}
+                  className="close-btn mt-5"
+                  icon
+                  onClick={() => handleRemove({ value: index })}
+                  type="button"
+                >
+                  <Icon name="close" />
+                </Button>
+              </Form.Field>
+            </GroupField>
+          ))}
+        </Grid.Column>
+        <Grid.Row>
+          <Grid.Column>
             <Button
-              aria-label={"Remove field"}
-              className="close-btn"
+              primary
               icon
-              onClick={() => handleRemove({ value: index })}
-              type="button"
+              onClick={(e) => {
+                e.preventDefault();
+                handleAdd();
+              }}
             >
-              <Icon name="close" />
+              <Icon name="plus" />
             </Button>
           </Grid.Column>
-          <Grid.Column width="15">
-            {importedComponent.component(index)}
-          </Grid.Column>
         </Grid.Row>
-        )}
-        <Grid.Row>
-          <Button
-            primary
-            icon
-            onClick={(e) => {
-              e.preventDefault();
-              handleAdd();
-            }}
-          >
-            <Icon name="plus" />
-          </Button>
-        </Grid.Row>
-    </Grid>
+      </Grid>
     </>
   );
+};
+
+ArrayWidget.propTypes = {
+  item_widget: PropTypes.string.isRequired,
+  item_props: PropTypes.object,
+  item_initial_value: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+  fieldPath: PropTypes.string.isRequired,
+  label: PropTypes.string,
 };
--- oarepo_ui/theme/assets/semantic-ui/templates/custom_fields/ComplexWidget.js
+++ oarepo_ui/theme/assets/semantic-ui/templates/custom_fields/ComplexWidget.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { Grid, Header } from "semantic-ui-react";
+import PropTypes from "prop-types";
 
 export async function importTemplate(widget) {
   let error;
   try {
     const module = await import(`/templates/custom_fields/${widget}.js`);
     return module.default;
-  } catch(e) {error = e}
+  } catch (e) {
+    error = e;
+  }
 
   const module = await import(`react-invenio-forms`);
   if (module[widget]) {
@@ -15,51 +18,48 @@ export async function importTemplate(widget) {
   throw new Error(`Widget ${widget} not found. First error ${error}`);
 }
 
-const ComplexWidget = (props) => {
-  const { fieldPath, nested, label } = props;
-
+const ComplexWidget = ({ fieldPath, nested, label }) => {
   const [importedComponents, setImportedComponents] = useState([]);
 
   useEffect(() => {
     async function loadImportedComponents() {
-      const importComponent = async ({field: nestedField, ui_widget: nestedWidget, props: nestedProps}) => {
+      const importComponent = async ({
+        field: nestedField,
+        ui_widget: nestedWidget,
+        props: nestedProps,
+      }) => {
         const NestedWidgetComponent = await importTemplate(nestedWidget);
         const p = {
           ...nestedProps,
-          fieldPath: fieldPath + '.' + nestedField
-        }
+          fieldPath: fieldPath + "." + nestedField,
+        };
         return {
           nestedId: nestedField,
-          nestedConstructor: () => <NestedWidgetComponent {...p}/>
-        }
+          nestedConstructor: () => <NestedWidgetComponent {...p} />,
+        };
       };
 
-      setImportedComponents(
-          await Promise.all(nested.map(importComponent))
-      )
+      setImportedComponents(await Promise.all(nested.map(importComponent)));
     }
-    loadImportedComponents()
+    loadImportedComponents();
   }, []);
 
   return (
     <>
-    <Header as="h3">{label}</Header>
-    <Grid>
-      <Grid.Column width={12}>
-        {
-          importedComponents.map(({nestedId, nestedConstructor}) => {
-              return (
-                 <Grid.Row key={nestedId}>
-                    {nestedConstructor()}
-                 </Grid.Row>
-              );
-            }
-          )
-        }
-      </Grid.Column>
-    </Grid>
+      <Header as="h3">{label}</Header>
+      <Grid>
+        <Grid.Column>
+          {importedComponents.map(({ nestedId, nestedConstructor }) => {
+            return <Grid.Row key={nestedId}>{nestedConstructor()}</Grid.Row>;
+          })}
+        </Grid.Column>
+      </Grid>
     </>
   );
 };
-
+ComplexWidget.propTypes = {
+  fieldPath: PropTypes.string.isRequired,
+  nested: PropTypes.array.isRequired,
+  label: PropTypes.string.isRequired,
+};
 export default ComplexWidget;
