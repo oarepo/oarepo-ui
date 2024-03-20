c0c73499e16e211410d4067199fd9f92dd96b9f5
ducica <dusanstojanovic256@gmail.com>
2024-03-20 13:08:34 +0100

    modified clear filters button

--- oarepo_ui/theme/assets/semantic-ui/js/oarepo_ui/search/ClearFiltersButton.jsx
+++ oarepo_ui/theme/assets/semantic-ui/js/oarepo_ui/search/ClearFiltersButton.jsx
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
 import { withState } from "react-searchkit";
 import { Button } from "semantic-ui-react";
 import { i18next } from "@translations/oarepo_ui/i18next";
 import PropTypes from "prop-types";
+import { SearchConfigurationContext } from "@js/invenio_search_ui/components";
 
+// in some cases, there are some permanent facets i.e. in requests open/closed, 
+// so we have button not remove those initial filters
 const ClearFiltersButtonComponent = ({
   updateQueryState,
   currentQueryState,
 }) => {
   const { filters } = currentQueryState;
+  const searchAppContext = useContext(SearchConfigurationContext);
+  const {
+    initialQueryState: { filters: initialFilters },
+  } = searchAppContext;
+
   return (
-    filters.length > 0 && (
+    filters.length > initialFilters?.length && (
       <Button
         name="clear"
         color="orange"
-        onClick={() => updateQueryState({ ...currentQueryState, filters: [] })}
+        onClick={() =>
+          updateQueryState({
+            ...currentQueryState,
+            filters: currentQueryState.filters.filter((f) =>
+              initialFilters.map((f) => f[0]).includes(f[0])
+            ),
+          })
+        }
         icon="delete"
         labelPosition="left"
         content={i18next.t("Clear all filters")}
