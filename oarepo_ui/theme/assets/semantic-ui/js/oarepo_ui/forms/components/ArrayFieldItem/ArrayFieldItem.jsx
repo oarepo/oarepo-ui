d4298c463afcc3b46add1ac8434c3f70754c7688
ducica <dusanstojanovic256@gmail.com>
2024-06-09 10:55:07 +0200

    fixed up array prop

--- oarepo_ui/theme/assets/semantic-ui/js/oarepo_ui/forms/components/ArrayFieldItem/ArrayFieldItem.jsx
+++ oarepo_ui/theme/assets/semantic-ui/js/oarepo_ui/forms/components/ArrayFieldItem/ArrayFieldItem.jsx
@@ -14,6 +14,7 @@ export const ArrayFieldItem = ({
   removeButtonProps,
   removeButtonLabelClassName,
   fieldPathPrefix,
+  array,
   ...uiProps
 }) => {
   const [highlighted, setHighlighted] = useState(false);
@@ -82,6 +83,7 @@ ArrayFieldItem.propTypes = {
   displayFirstInputRemoveButton: PropTypes.bool,
   removeButtonLabelClassName: PropTypes.string,
   fieldPathPrefix: PropTypes.string.isRequired,
+  array: PropTypes.array,
 };
 
 ArrayFieldItem.defaultProps = {
