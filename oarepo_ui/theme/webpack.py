2b2be01767a07cae4e768c6f9cadb8f963624bd9
Martin Čorovčák <martino.coro@gmail.com>
2024-06-14 17:57:04 +0200

    fix embedded layout to templates

--- oarepo_ui/theme/webpack.py
+++ oarepo_ui/theme/webpack.py
@@ -59,7 +59,8 @@ else:
         "react-datepicker": "^4.21.0",
         "edtf": "^4.0.0",
         "html-entities": "2.5.2",
-        "sanitize-html": "2.13.0"
+        "sanitize-html": "2.13.0",
+        "@iframe-resizer/child": "^5.0.2",
     }
     aliases = {}
 
@@ -76,6 +77,7 @@ theme = WebpackThemeBundle(
                 "oarepo_ui_theme": "./js/oarepo_ui/theme.js",
                 "oarepo_ui_components": "./js/oarepo_ui/custom-components.js",
                 "copy_to_clipboard": "./js/oarepo_ui/components/clipboard.js",
+                "iframe_resizer_child": "./js/oarepo_ui/components/iframe-resizer-child.js",
             },
             dependencies=dependencies,
             devDependencies={"eslint-plugin-i18next": "^6.0.3"},
