2b8a91d61d06582f45b1df5c87eacb67717eb4b4
Miroslav Bauer <mirekys@gmail.com>
2024-11-08 14:19:01 +0100

    feat(overrides): implement lazy overridable closure to load component when needed

--- oarepo_ui/theme/assets/semantic-ui/js/oarepo_ui/overridable-registry.js
+++ oarepo_ui/theme/assets/semantic-ui/js/oarepo_ui/overridable-registry.js
@@ -2,16 +2,32 @@ import * as React from 'react';
 import { overrideStore } from "react-overridable";
 import { getInputFromDOM } from "./util";
 import { importTemplate } from "@js/invenio_theme/templates";
+import PropTypes from 'prop-types'
 
-const LazyOverriddenComponent = ({ importString }) => {
-    //     TODO: try to import and render component module once rendered
-    const Component = importTemplate(importString);
-    console.log({Component})
+function lazyOverridable (importString) {
+    function LazyOverride ({children, ...props}) {
+        const Override = importTemplate(importString);
+
+        const name = Override.displayName || Override.name;
+        Override.displayName = `LazyOverride(${name})`;
+
+        Override.propTypes = {
+            children: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
+        };
+        Override.defaultProps = {
+            children: null,
+        };
+
+        return <Override {...props}>{children}</Override>
+    }
+
+    return LazyOverride
 }
+
 
 const reactOverrides = getInputFromDOM("react-overrides");
 Object.entries(reactOverrides).forEach(
     ([overridableId, importString]) => overrideStore.add(
-        overridableId, <LazyOverriddenComponent importString={importString} />
+        overridableId, lazyOverridable(importString)
     ));
 console.debug("Global React component overrides:", overrideStore.getAll())
