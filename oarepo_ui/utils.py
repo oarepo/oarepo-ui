91540ba81c840cb42dc0eed6e1c968ae9d286a3c
ducica <dusanstojanovic256@gmail.com>
2024-11-18 12:26:17 +0100

    cached deposit page view permissions to session

--- oarepo_ui/utils.py
+++ oarepo_ui/utils.py
@@ -1,6 +1,8 @@
 from marshmallow import Schema, fields
 from marshmallow.schema import SchemaMeta
 from marshmallow_utils.fields import NestedAttribute
+from flask import session, current_app, g
+from invenio_base.utils import obj_or_import_string
 
 
 def dump_empty(schema_or_field):
@@ -28,3 +30,38 @@ def dump_empty(schema_or_field):
     if isinstance(schema_or_field, fields.Dict):
         return {}
     return None
+
+
+view_deposit_page_permission_key = "view_deposit_page_permission"
+
+
+# check if the user has the right to access the deposit page and store the results in session
+def can_view_deposit_page():
+    permission_to_deposit = False
+    if view_deposit_page_permission_key in session:
+        return session[view_deposit_page_permission_key]
+    else:
+        repository_search_resources = current_app.config.get("GLOBAL_SEARCH_MODELS", [])
+        if not repository_search_resources:
+            return False
+        for search_resource in repository_search_resources:
+            search_resource_service = search_resource.get("model_service", None)
+            search_resource_config = search_resource.get("service_config", None)
+            if search_resource_service and search_resource_config:
+                try:
+                    service_def = obj_or_import_string(search_resource_service)
+                    service_cfg = obj_or_import_string(search_resource_config)
+                    service = service_def(service_cfg())
+                    permission_to_deposit = service.check_permission(
+                        g.identity, "view_deposit_page", record=None
+                    )
+                    if permission_to_deposit:
+                        break
+                except ImportError:
+                    continue
+        session[view_deposit_page_permission_key] = permission_to_deposit
+        return permission_to_deposit
+
+
+def clear_view_deposit_page_permission_from_session(*args, **kwargs):
+    session.pop(view_deposit_page_permission_key, None)
