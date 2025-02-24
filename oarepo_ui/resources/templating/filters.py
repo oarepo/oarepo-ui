e64fcf426e15fa3bb36bdde61c85e379a8457dcd
Martin Čorovčák <martino.coro@gmail.com>
2025-02-24 14:28:52 +0100

    Implement statistics section into detail sidebar

--- oarepo_ui/resources/templating/filters.py
+++ oarepo_ui/resources/templating/filters.py
@@ -1,4 +1,6 @@
 from oarepo_ui.resources.templating.data import EMPTY_FIELD_DATA, FieldData
+from babel.numbers import format_compact_decimal, format_decimal
+from flask import current_app
 
 
 def id_filter(x):
@@ -96,3 +98,31 @@ def ijoin_filter(values, separator):
         else:
             ret.append(str(val))
     return separator.join(ret)
+
+
+def localize_number(value):
+    """Format number according to locale value."""
+    locale_value = current_app.config.get("BABEL_DEFAULT_LOCALE")
+    number = int(value)
+    return format_decimal(number, locale=locale_value)
+
+
+def compact_number(value, max_value):
+    """Format long numbers."""
+    locale_value = current_app.config.get("BABEL_DEFAULT_LOCALE")
+    number = int(value)
+    decimals = 0
+
+    if number > max_value:
+        decimals = 2
+    return format_compact_decimal(
+        int(value), format_type="short", locale=locale_value, fraction_digits=decimals
+    )
+
+
+def truncate_number(value, max_value):
+    """Make number compact if too long."""
+    number = localize_number(value)
+    if int(value) > max_value:
+        number = compact_number(value, max_value=1_000_000)
+    return number
