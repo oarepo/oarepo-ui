d32e8286e62358c6d5ab6a85574f485058a314da
ducica <dusanstojanovic256@gmail.com>
2025-03-31 09:40:50 +0200

    fixed failed prop types for pagination

--- oarepo_ui/theme/assets/semantic-ui/js/oarepo_ui/forms/components/FundingField/FundingField.jsx
+++ oarepo_ui/theme/assets/semantic-ui/js/oarepo_ui/forms/components/FundingField/FundingField.jsx
@@ -10,24 +10,38 @@ import { Pagination } from "semantic-ui-react";
 
 const storeComponents = overrideStore.getAll();
 
-const SmallPagination = (props) => (
-  <Pagination
-    {...props}
-    size="tiny"
-    firstItem={null}
-    lastItem={null}
-    boundaryRange={0}
-    siblingRange={1}
-    showFirst={null}
-    showLast={null}
-    activePage={props.currentPage}
-    onPageChange={(_, { activePage }) => props.onPageChange(activePage)}
-  />
-);
+const SmallPagination = ({
+  currentPage,
+  totalResults,
+  options,
+  currentSize,
+  onPageChange,
+  maxTotalResults = 10000,
+}) => {
+  const maxTotalPages = Math.floor(maxTotalResults / currentSize);
+  const pages = Math.ceil(totalResults / currentSize);
+  const totalDisplayedPages = Math.min(pages, maxTotalPages);
+  return (
+    <Pagination
+      activePage={currentPage}
+      size="tiny"
+      totalPages={totalDisplayedPages}
+      firstItem={null}
+      lastItem={null}
+      boundaryRange={0}
+      siblingRange={3}
+      onPageChange={(_, { activePage }) => onPageChange(activePage)}
+    />
+  );
+};
 
 SmallPagination.propTypes = {
-  currentPage: PropTypes.number,
-  onPageChange: PropTypes.func,
+  currentPage: PropTypes.number.isRequired,
+  totalResults: PropTypes.number.isRequired,
+  options: PropTypes.object.isRequired,
+  currentSize: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  maxTotalResults: PropTypes.number,
 };
 
 export const FundingField = ({
--- oarepo_ui/theme/assets/semantic-ui/js/oarepo_ui/forms/components/FundingField/util.js
+++ oarepo_ui/theme/assets/semantic-ui/js/oarepo_ui/forms/components/FundingField/util.js
@@ -1,13 +1,13 @@
 export const deserializeFunder = (funder) => {
   return {
-    id: funder.id,
-    name: funder.name,
-    ...(funder.title_l10n && { title: funder.title_l10n }),
-    ...(funder.country && { country: funder.country }),
-    ...(funder.country_name && {
+    id: funder?.id,
+    name: funder?.name,
+    ...(funder?.title_l10n && { title: funder.title_l10n }),
+    ...(funder?.country && { country: funder.country }),
+    ...(funder?.country_name && {
       country_name: funder.country_name,
     }),
-    ...(funder.identifiers && {
+    ...(funder?.identifiers && {
       identifiers: funder.identifiers,
     }),
   };
