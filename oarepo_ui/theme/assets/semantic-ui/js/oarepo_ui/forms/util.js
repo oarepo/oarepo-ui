2eed65a02f7b0833e1e27917b758e717b1cb97f7
Dusan Stojanovic <dusanstojanovic256@gmail.com>
2025-10-06 13:08:37 +0200

    fix: correct hidden input name for record files

--- oarepo_ui/theme/assets/semantic-ui/js/oarepo_ui/forms/util.js
+++ oarepo_ui/theme/assets/semantic-ui/js/oarepo_ui/forms/util.js
@@ -13,7 +13,7 @@ export function parseFormAppConfig(rootElementId = "deposit-form") {
   const record = getInputFromDOM("deposits-record");
   const formConfig = getInputFromDOM("deposits-config");
   const recordPermissions = getInputFromDOM("deposits-permissions");
-  const files = getInputFromDOM("deposits-files");
+  const files = getInputFromDOM("deposits-record-files");
   const links = getInputFromDOM("deposits-links");
   return { rootEl, record, formConfig, recordPermissions, files, links };
 }
@@ -126,10 +126,10 @@ export function toModelPath(path) {
       return part;
     } else if (!Number.isNaN(Number.parseInt(part))) {
       return `child.children`;
-    } else if (!Number.isNaN(Number.parseInt(array[index + 1]))) {
-      return part;
-    } else {
+    } else if (Number.isNaN(Number.parseInt(array[index + 1]))) {
       return `${part}.children`;
+    } else {
+      return part;
     }
   });
   // Join the transformed parts back into a single string
