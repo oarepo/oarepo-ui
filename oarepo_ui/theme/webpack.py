67da34833928a829406fa83ea4f600e4032108f9
Ondřej Ruml <ruml.ondrej@proton.me>
2025-11-20 16:53:49 +0100

    fix: Removed custom javascript for record export in record detail HTML template and in Webpack Theme Bundle.

--- oarepo_ui/theme/webpack.py
+++ oarepo_ui/theme/webpack.py
@@ -33,7 +33,6 @@ theme = WebpackThemeBundle(
                 "oarepo_ui_theme": "./js/oarepo_ui/theme.js",
                 "oarepo_ui_components": "./js/oarepo_ui/custom-components.js",
                 "copy_to_clipboard": "./js/oarepo_ui/components/clipboard.js",
-                "record_export": "./js/oarepo_ui/components/record-export.js",
                 "record_sharing": "./js/oarepo_ui/components/record-sharing.js",
             },
             "dependencies": {
