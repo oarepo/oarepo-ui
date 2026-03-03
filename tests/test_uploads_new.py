fa6d37b16e615f404e74ad8b19ddb9578d7d943d
Dusan Stojanovic <dusanstojanovic256@gmail.com>
2026-03-03 12:48:52 +0100

    added tests

---
+++ tests/test_uploads_new.py
@@ -0,0 +1,30 @@
+#
+# Copyright (c) 2025 CESNET z.s.p.o.
+#
+# This file is a part of oarepo-ui (see https://github.com/oarepo/oarepo-ui).
+#
+# oarepo-ui is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+"""Tests for the generic /uploads/new route in oarepo_ui.views (single model)."""
+
+from __future__ import annotations
+
+
+def test_uploads_new_single_model_redirect(app, logged_client, users, extra_entry_points):
+    """Test that /uploads/new redirects to model-specific page when single model exists."""
+    client = logged_client(users[0])
+
+    with client.get("/uploads/new") as resp:
+        assert resp.status_code == 302
+        assert resp.location.endswith("/simple-model/uploads/new")
+
+
+def test_uploads_new_single_model_with_community(app, logged_client, users, extra_entry_points):
+    """Test that /uploads/new preserves community parameter in redirect."""
+    client = logged_client(users[0])
+
+    with client.get("/uploads/new?community=test-community") as resp:
+        assert resp.status_code == 302
+        assert "/simple-model/uploads/new" in resp.location
+        assert "community=test-community" in resp.location
