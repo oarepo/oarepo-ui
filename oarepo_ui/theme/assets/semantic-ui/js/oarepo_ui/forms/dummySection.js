8b2cf15195a83ba31a8545877d03d3e3d2ed8cec
Dusan Stojanovic <dusanstojanovic256@gmail.com>
2026-04-23 10:59:06 +0200

    added serializer for empty model

---
+++ oarepo_ui/theme/assets/semantic-ui/js/oarepo_ui/forms/dummySection.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { i18next } from "@translations/i18next";
+
+export const DummySection = {
+  key: "general-information",
+  label: i18next.t("General information"),
+  component: () => {
+    return (
+      <p>
+        {i18next.t("Please define your sections.")}{" "}
+        <a
+          href="https://nrp-cz.github.io/docs/customize/model_ui/deposit#defining-sections"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {i18next.t("See the documentation")}
+        </a>
+      </p>
+    );
+  },
+  includesPaths: [],
+};
--- oarepo_ui/theme/assets/semantic-ui/js/oarepo_ui/forms/index.js
+++ oarepo_ui/theme/assets/semantic-ui/js/oarepo_ui/forms/index.js
@@ -2,3 +2,4 @@ export * from "./contexts";
 export * from "./util";
 export * from "./hooks";
 export * from "./components";
+export * from "./dummySection";
